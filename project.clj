559a0e915780c54d821d36c1395ed38a86dcd158
vshishkov <vshishkov@amberdata.ru>
2019-02-14 15:32:45 +0300

    publish to clojars

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject cljs-add-script "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "dynamically add script tag to selected DOM node"
+  :url "https://github.com/evilsneer/cljs-add-script"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.439"
                   :scope "provided"]
-                 [prismatic/dommy "1.1.0"]]
-  :repl-options {:init-ns cljs-add-script.core})
+                 [prismatic/dommy "1.1.0"]])
