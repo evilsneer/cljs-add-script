45e272e375ef1fb2953e05109ae90c1708577794
vshishkov <vshishkov@amberdata.ru>
2019-02-19 17:03:30 +0300

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-add-script "0.1.0"
+(defproject cljs-add-script "0.1.1"
   :description "dynamically add script tag to selected DOM node"
   :url "https://github.com/evilsneer/cljs-add-script"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
